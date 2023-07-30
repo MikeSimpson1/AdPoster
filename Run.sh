1d0faae6438b3ddab2ce9ae8b3c57069c837e2b3
MikeSimpson1 <petworker6@gmail.com>
2023-07-30 17:50:15 -0400

    Removed unused files

--- Run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-python3 home/mike/Desktop/Projects/CameraApplication.py
-sudo scp -r /home/mike/Desktop/Projects/Images Mike@192.168.11.5:Desktop/bookimages
--- Startup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-from gpiozero import Button
-import os
-button = Button(21)
-while True:
-    if button.is_pressed:
-        os.system("/home/mike/Desktop/Projects/Run.sh")
