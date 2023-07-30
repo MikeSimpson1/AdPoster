a7ff2150683bbc67ba20e8af2f1e0ad05ef97bc1
MikeSimpson1 <petworker6@gmail.com>
2023-07-30 17:52:58 -0400

    Further changes

--- RaspberryPi/RPiRunSequence.sh
+++ RaspberryPi/RPiRunSequence.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-python3 home/mike/Desktop/Projects/CameraApplication.py
+python3 home/mike/Desktop/Projects/RaspberryPi/CameraApplication.py
 scp -r /home/mike/Desktop/Projects/Images Mike@192.168.11.5:Desktop/Projects/MarketplaceAdPoster
 ssh 'mike@192.168.11.5' 'powershell schtasks /run /tn MarketplaceAdPoster'
